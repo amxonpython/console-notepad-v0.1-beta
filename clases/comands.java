db6b349b9da69e6e44caa54c133d84fc1d9b254a
lampa <134073971+amxonpython@users.noreply.github.com>
2026-04-30 14:00:08 +0300

    Update comands.java

--- clases/comands.java
+++ clases/comands.java
@@ -5,7 +5,8 @@ public class comands {
         System.out.println("exit - выход");
         System.out.println("saved - сохранить текст");
         System.out.println("take out - вывести текст");
-        System.out.println("take out - вывести весь текст");
+        System.out.println("take out all - вывести весь текст");
+        System.out.println("clear - очистить лист");
         System.out.println();
     }
     public static void error_not_command(){
@@ -13,5 +14,8 @@ public class comands {
     }
     public static void Too_big_id(){
         System.out.println("Error: вы ввели слишком большое значение");
+    }
+    public static void clear_list(){
+        System.out.println("лист очищен");
     }
 }
